f746b391c0ddb0b95ea7dffb87c027d687316eb0
PramodKafle <pramodkafle1985@gmail.com>
2021-09-24 15:43:11 -0400

    my first hello project

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello pramod");
+        System.out.println("hello Arun");
+        System.out.println("hello Niranjana");
+        System.out.println("hello Avay");
+    }
+}
